2b231da2fe2456d7508136f8170fd86cfaf89ecc
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-15 23:03:30 +0700

    Добавление списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> employees = new List<Employee>()
+        {
+            new Employee() { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronomic = "иванович", Age = 27 },
+            new Employee() { Id = 2, LastName = "Петров", FirstName = "Петр", Patronomic = "Петрович", Age = 30 },
+            new Employee() { Id = 3, LastName = "Сидоров", FirstName = "Сидр", Patronomic = "Сидорович", Age = 27 }
+        };
+
         public IActionResult Index()
         {
             //return Content("Data from new controller");
