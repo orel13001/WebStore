b6663f9000338985b963b13a191b379e7eb1bc4f
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-15 22:43:22 +0700

    Добавление первого контроллера

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Data from new controller");
+        }
+    }
+}
