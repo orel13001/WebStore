736f2ed553b099233e6df80d025aa3c2341643c1
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-15 23:19:12 +0700

    Добавление представления для вывода сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> employees = new List<Employee>()
+        private static readonly List<Employee> _Employees = new List<Employee>()
         {
             new Employee() { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronomic = "иванович", Age = 27 },
             new Employee() { Id = 2, LastName = "Петров", FirstName = "Петр", Patronomic = "Петрович", Age = 30 },
@@ -21,6 +21,12 @@ namespace WebStore.Controllers
         public string ConfiguredAction(string id)
         {
             return $"Hellow World {id}!";
+        }
+
+
+        public IActionResult Employees()
+        {
+            return View(_Employees);
         }
     }
 
