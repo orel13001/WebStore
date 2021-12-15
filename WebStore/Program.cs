54f4c0e5fd79d80ecd6d6eea5a8581763732f055
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-15 22:34:38 +0700

    Добавление тестового исключения

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,11 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var servises = builder.Services;
+servises.AddControllersWithViews(); // добавление инфраструктуры MVC (контроллеры и представления)
+
+
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); //позволяет перехватывать все исключения приложения
+}
+
 
 //Загрузка инфы из файла конфигурации
 
 //var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
 
+
+
+
 app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () =>
+    {
+        throw new ApplicationException("Ошибка в программе"); //генерация исключения для проверки диагностики
+    });
 
 app.Run();
