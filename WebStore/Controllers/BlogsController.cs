fc3358868a5e45ee83c04c6f2b43ddce8885f9fb
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-19 21:23:57 +0700

    Добавление блогов

---
+++ WebStore/Controllers/BlogsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Blog() => View();
+    }
+}
