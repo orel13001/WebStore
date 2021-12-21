89a3d999c9cef0e47a6aee2ed84d725069a0a162
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-21 22:40:46 +0700

    Заготовка методов редактирования в контроллере сотрудников.

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Models;
+using WebStore.ViewModels;
 using WebStore.Data;
 
 namespace WebStore.Controllers
@@ -28,5 +29,46 @@ namespace WebStore.Controllers
                 return NotFound();
             return View(employee);
         }
+
+        //public IActionResult Create() => View();
+
+        /// <summary>
+        /// Отправка формы для редактирования сотрудника
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Edit(int id)
+        {
+            var employee = _Employees.FirstOrDefault(o => id == o.Id);
+
+            if (employee == null)
+                return NotFound();
+
+            var model = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Age = employee.Age,
+                Patronymic = employee.Patronomic,
+                
+            };
+
+            return View(model);
+        }
+
+        /// <summary>
+        /// Приём результата изменения в форме и его обработка
+        /// </summary>
+        /// <param name="Model"></param>
+        /// <returns></returns>
+        public IActionResult Edit(EmployeeEditViewModel Model)
+        {
+            //Обработка модели...
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) => View();
     }
 }
---
+++ WebStore/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    /// <summary>
+    /// ViewModel сотрудника - оболочка для действия "редактирование" сотрудника
+    /// </summary>
+    public class EmployeeEditViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Age { get; set; }
+    }
+}
