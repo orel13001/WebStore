203fcfa0e6d4fb6ec9598533281b14c6520d0997
orel13001 <68144818+orel13001@users.noreply.github.com>
2021-12-27 22:38:06 +0700

    Добавление описания товара

---
+++ WebStore.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿
+
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Product : NamedEntity, IOrderEntity
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
