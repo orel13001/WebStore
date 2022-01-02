c4e082f6b4d83f1a1b568ba7d2dc84bc9a49cdc7
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-02 21:41:07 +0700

    Подготовка модели данных для её размещения в БД

--- WebStore.Domain/Entities/Base/NamedEntity.cs
+++ WebStore.Domain/Entities/Base/NamedEntity.cs
@@ -1,9 +1,11 @@
-﻿using WebStore.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities.Base
 {
     public abstract class NamedEntity : Entity, INamedEntity
     {
+        [Required]//свойство является обязательным
         public string Name { get; set; }
     }
 }
--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,16 +1,27 @@
 ﻿
-
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Index(nameof(Name))]
     public class Product : NamedEntity, IOrderEntity
     {
         public int Order { get; set; }
         public int SectionId { get; set; }
+
+        [ForeignKey(nameof(SectionId))]
+        public Section Section { get; set; }
         public int? BrandId { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
+
         public string ImageUrl { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
 
 
