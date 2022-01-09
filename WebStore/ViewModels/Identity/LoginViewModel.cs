8818c975d5e47da6f42112baab0c60d4e109fe90
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-09 11:06:44 +0700

    Модели-представления для регистрации и входа.

---
+++ WebStore/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить")]
+        public bool RememberMe { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string ReturnUrl { get; set; }
+    }
+}
