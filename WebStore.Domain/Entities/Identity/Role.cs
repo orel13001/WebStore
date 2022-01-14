e798b7b4e8dce7fbd221bb06e0003a60c35ffbad
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-14 21:38:58 +0700

    Создание пользователя-администратора при создании БД

--- WebStore.Domain/Entities/Identity/Role.cs
+++ WebStore.Domain/Entities/Identity/Role.cs
@@ -4,6 +4,7 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class Role : IdentityRole
     {
-
+        public const string Administrotors = "Administrotors";
+        public const string Users = "Users";
     }
 }
