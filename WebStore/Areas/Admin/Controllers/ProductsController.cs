07dabd043573fb92c689dbf59712c49e8985be9a
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-17 22:55:41 +0700

    Логика контроллера управления товаром

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Areas.Admin.ViewModel;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Services.Interfaces;
 
@@ -21,5 +22,93 @@ namespace WebStore.Areas.Admin.Controllers
             var products = _productData.GetProducts();
             return View(products);
 		}
+
+        public IActionResult Edit(int id)
+		{
+            var product = _productData.GetProductById(id);
+
+			if (product is null)
+			{
+                return NotFound();
+			}
+
+            return View(new EditProductViewModel()
+			{
+                Id = product.Id,
+                Name = product.Name,
+                Order = product.Order,
+                SectionId = product.SectionId,
+                Section = product.Section.Name,
+                Brand = product.Brand?.Name,
+                BrandId = product.Brand?.Id,
+                ImageUrl = product.ImageUrl,
+                Price = product.Price,
+			});
+		}
+
+        [HttpPost]
+        public IActionResult Edit(EditProductViewModel viewModel)
+		{
+            if(!ModelState.IsValid)
+                return View(viewModel);
+
+   //         var product = _productData.GetProductById(viewModel.Id);
+			//if (product is null)
+			//{
+   //             return NotFound();
+			//}
+
+   //         product.Name = viewModel.Name;
+   //         product.Order = viewModel.Order;
+   //         product.Price = viewModel.Price;
+   //         product.ImageUrl = viewModel.ImageUrl;
+
+   //         var brand = _productData.GetBrandById(viewModel.BrandId ?? -1);
+   //         var section = _productData.GetSectionById(viewModel.SectionId);
+
+   //         product.Brand = brand;
+   //         product.Section = section;
+   //   обновить, используя сервис _productData
+   //         //_productData.Update(product);
+
+            return RedirectToAction(nameof(Index));
+		}
+
+        public IActionResult Delete (int id)
+		{
+            var product = _productData.GetProductById(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return View(new EditProductViewModel()
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Order = product.Order,
+                SectionId = product.SectionId,
+                Section = product.Section.Name,
+                Brand = product.Brand?.Name,
+                BrandId = product.Brand?.Id,
+                ImageUrl = product.ImageUrl,
+                Price = product.Price,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+		{
+            var product = _productData.GetProductById(id);
+
+            if (product is null)
+                return NotFound();
+
+            //   удалить, используя сервис _productData
+            //_productData.Delete(product);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
