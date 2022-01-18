fee3efff4ff569b9f9d6a062ccbf82b2ed3fa300
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-18 22:59:20 +0700

    Профиль пользователя

--- WebStore/Controllers/UserProfileController.cs
+++ WebStore/Controllers/UserProfileController.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     public class UserProfileController : Controller
     {
         public IActionResult Index() => View();
+
+        public async Task<IActionResult> Orders([FromServices] IOrderService orderService)
+        {
+            var orders = await orderService.GetUserOrdersAsync(User.Identity!.Name!);
+
+            return View(orders.Select(ord => new UserOrderViewModel()
+            {
+                Id = ord.Id,
+                Address = ord.Address,
+                Date = ord.Date,
+                Description = ord.Description,
+                Phone = ord.Phone,
+                TotalPrice = ord.TotalPrice,
+            }));
+        }
     }
 }
