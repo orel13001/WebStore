f5464d53962012a7d9fe6b359576d364af7a71f3
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-21 23:03:15 +0700

    Класс базового клиента

---
+++ Services/WebStore.WebAPI.Clients/Values/ValuesClient.cs
@@ -0,0 +1,39 @@
+﻿
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.WebAPI.Clients.Values
+{
+    public class ValuesClient : IValuesService
+    {
+        private HttpClient _Client;
+        public void Add(string value)
+        {
+            
+        }
+
+        public int Count()
+        {
+            
+        }
+
+        public void Delete(int Id)
+        {
+            
+        }
+
+        public void Edit(int Id, string Value)
+        {
+            
+        }
+
+        public string GetById(int Id)
+        {
+            
+        }
+
+        public IEnumerable<string> GetValues()
+        {
+            
+        }
+    }
+}
