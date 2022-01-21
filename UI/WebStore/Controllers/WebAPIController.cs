fc791b476a9332899fa5b33a327699a5d8a4399c
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-21 23:50:03 +0700

    Визуальное управление клиентом тестового приложения

---
+++ UI/WebStore/Controllers/WebAPIController.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.TestAPI;
+
+namespace WebStore.Controllers
+{
+    public class WebAPIController : Controller
+    {
+        private readonly IValuesService _valuesService;
+
+        public WebAPIController(IValuesService valuesService) => _valuesService = valuesService;
+
+
+        public IActionResult Index()
+        {
+            var values = _valuesService.GetValues();
+            return View(values);
+        }
+    }
+}
