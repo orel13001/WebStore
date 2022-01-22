ddd02c4cf72d7c3a91917cb83386c8e0671b0b95
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-22 22:57:59 +0700

    Регистрация сервисов бизнес-логики в проекте хоста WebAPI

--- Services/WebStore.WebAPI/Program.cs
+++ Services/WebStore.WebAPI/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces.Services;
+using WebStore.Services.Services;
+using WebStore.Services.Services.InSQL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
@@ -5,6 +13,40 @@ var services = builder.Services;
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+
+services.AddDbContext<WebStoreDB>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+services.AddTransient<IDbInitializer, DbInitializer>();
+
+services.AddIdentity<User, Role>()              //Добавление сервиса идентификации
+    .AddEntityFrameworkStores<WebStoreDB>()     //Указание источника данных
+    .AddDefaultTokenProviders();                //указание токена по умолчанию
+
+//конфигурация сервиса идентификации
+services.Configure<IdentityOptions>(opt =>
+{
+#if DEBUG
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 3;
+    opt.Password.RequiredUniqueChars = 3;
+#endif
+
+    opt.User.RequireUniqueEmail = false;
+    opt.User.AllowedUserNameCharacters = "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+
+    opt.Lockout.AllowedForNewUsers = false;
+    opt.Lockout.MaxFailedAccessAttempts = 10;
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+});
+
+services.AddScoped<IProductData, InSqlProductData>();
+services.AddScoped<IEmployeesData, InSqlEmployeeData>();
+//servises.AddScoped<ICartService, InCookiesCartService>();
+services.AddScoped<IOrderService, InSqlOrderService>();
 
 var app = builder.Build();
 
