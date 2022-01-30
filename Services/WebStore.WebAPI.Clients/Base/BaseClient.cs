7fa2e27a794445e4dcf8f5d82b1b8e957816fcd8
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-01-30 22:59:56 +0700

    Добавление в асинхронные методы флагов отмены асинхронных операций

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WebStore.WebAPI.Clients.Base
 {
-    public abstract class BaseClient
+    public abstract class BaseClient : IDisposable
     {
         protected HttpClient Http { get; }
 
@@ -21,37 +21,60 @@ namespace WebStore.WebAPI.Clients.Base
 
 
         protected T? Get<T>(string url) => GetAsync<T>(url).Result;
-        protected async Task<T?> GetAsync<T> (string url)
+        protected async Task<T?> GetAsync<T> (string url, CancellationToken Cancel = default)
         {
-            var response = await Http.GetAsync(url).ConfigureAwait(false);
+            var response = await Http.GetAsync(url, Cancel).ConfigureAwait(false);
             return await response
                 .EnsureSuccessStatusCode()
                 .Content
-                .ReadFromJsonAsync<T>();
+                .ReadFromJsonAsync<T>(cancellationToken: Cancel);
         }
 
 
         protected HttpResponseMessage Post<T>(string url, T value) => PostAsync<T>(url, value).Result;
-        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value)
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value, CancellationToken Cancel = default)
         {
-            var response = await Http.PostAsJsonAsync(url, value).ConfigureAwait(false);
+            var response = await Http.PostAsJsonAsync(url, value, Cancel).ConfigureAwait(false);
             return response.EnsureSuccessStatusCode();
         }
 
         protected HttpResponseMessage Put<T>(string url, T value) => PutAsync<T>(url, value).Result;
-        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value)
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value, CancellationToken Cancel = default)
         {
-            var response = await Http.PutAsJsonAsync(url, value).ConfigureAwait(false);
+            var response = await Http.PutAsJsonAsync(url, value, Cancel).ConfigureAwait(false);
             return response.EnsureSuccessStatusCode();
         }
 
 
         protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
-        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default)
         {
-            var response = await Http.DeleteAsync(url).ConfigureAwait(false);
+            var response = await Http.DeleteAsync(url, Cancel).ConfigureAwait(false);
             return response;
         }
 
+        public void Dispose()
+        {
+            Dispose(true);
+            //GC.SuppressFinalize(this);  // Если есть финализатор, то для текущего объекта отключаем финализацию в сборщике мусора
+        }
+
+        //~BaseClient() => Dispose(false);
+
+
+        protected bool _Dispose;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Dispose)
+                return;
+            _Dispose = true;
+
+            if(disposing)
+            {
+                //освобождаем управляемые ресурсы
+            }
+
+            // освобождаем неуправляемые ресурсы
+        }
     }
 }
