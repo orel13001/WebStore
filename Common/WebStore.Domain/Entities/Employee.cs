e4c1731d804bf2b3a7813b2dbc81f25d4c3cad3f
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-02-01 15:47:28 +0700

    Настройка документации

--- Common/WebStore.Domain/Entities/Employee.cs
+++ Common/WebStore.Domain/Entities/Employee.cs
@@ -7,11 +7,30 @@ using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities
 {
+
+    /// <summary>
+    /// Сотрудник
+    /// </summary>
     public class Employee : Entity
     {
+        /// <summary>
+        /// Фамиля
+        /// </summary>
         public string LastName { get; set; }
+
+        /// <summary>
+        /// Имя
+        /// </summary>
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
         public string? Patronomic { get; set; }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
         public int Age { get; set; }
     }
 }
--- Services/WebStore.WebAPI/Controllers/EmployeesAPIController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesAPIController.cs
@@ -18,6 +18,11 @@ namespace WebStore.WebAPI.Controllers
         private readonly IEmployeesData _employeesData;
         public EmployeesAPIController(IEmployeesData employeesData) => _employeesData = employeesData;
 
+
+        /// <summary>
+        /// Получение всех сотрудников
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Get()
         {
@@ -25,7 +30,15 @@ namespace WebStore.WebAPI.Controllers
             return Ok(employees);
         }
 
+
+        /// <summary>
+        /// Получение сотрудника по id
+        /// </summary>
+        /// <param name="id">Идентификатор сотрудника</param>
+        /// <returns></returns>
         [HttpGet("{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Employee))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(int id)
         {
             var employee = _employeesData.GetById(id);
@@ -35,21 +48,42 @@ namespace WebStore.WebAPI.Controllers
             return Ok(employee);
         } 
 
+
+        /// <summary>
+        /// Добавление нового сотрудника
+        /// </summary>
+        /// <param name="employee">Добавляемый сотрудник</param>
+        /// <returns></returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Employee))]
         public IActionResult Add(Employee employee)
         {
             var id = _employeesData.Add(employee);
             return CreatedAtAction(nameof(GetById), new{ id = id}, employee);
         }
 
+
+        /// <summary>
+        /// Изменение данных сотрудника
+        /// </summary>
+        /// <param name="employee">Структура с инфоормацией о сотруднике</param>
+        /// <returns></returns>
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
         public IActionResult Update(Employee employee)
         {
             var success = _employeesData.Edit(employee);
             return Ok(success);
         }
 
+        /// <summary>
+        /// Удаление сотрудника
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
         public IActionResult Delete(int id)
         {
             var result = _employeesData.Delete(id);
