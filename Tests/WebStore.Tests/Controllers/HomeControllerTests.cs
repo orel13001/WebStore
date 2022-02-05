f61dfbba3014e40a16097244044f35f595f34f91
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-02-06 00:33:40 +0700

    Метод тестирования с фейковой реализацией интерыейса

--- Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using WebStore.Controllers;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
 using Assert = Xunit.Assert;
 
 
@@ -10,16 +18,42 @@ namespace WebStore.Tests.Controllers
     public class HomeControllerTests
     {
         [TestMethod]
+        public void Index_returns_view()
+        {
+            var product_data_mock = new Mock<IProductData>();
+            product_data_mock.Setup(s => s.GetProducts(It.IsAny<ProductFilter>()))
+                .Returns<ProductFilter>(f => Enumerable.Empty<Product>());
+            
+            var controller = new HomeController();            
+            IProductData productData = product_data_mock.Object;
+            var result = controller.Index(productData);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+
+        [TestMethod]
         public void ConfiguredAction_Returns_string_value()
         {
+            //A-A-A = Arrange - Act - Assert
+
+            #region Arrange
             const string id = "123";
             //const string value_1 = "qwer";
             const string expected_string = $"Hellow World {id}!";
+            #endregion
 
+            #region Act
             var controller = new HomeController();
             var actual_string = controller.ConfiguredAction(id);
+            #endregion
 
-            Assert.Equal(actual_string, expected_string);
+            #region Assert
+            Assert.Equal(actual_string, expected_string); 
+            #endregion
         }
+
+
+
     }
 }
