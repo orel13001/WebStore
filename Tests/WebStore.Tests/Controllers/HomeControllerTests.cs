97fada3bf0aa7f79214e77ab7fed18bae843252c
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-02-06 00:13:20 +0700

    первый модульный тест

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using WebStore.Controllers;
+using Assert = Xunit.Assert;
+
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void ConfiguredAction_Returns_string_value()
+        {
+            const string id = "123";
+            //const string value_1 = "qwer";
+            const string expected_string = $"Hellow World {id}!";
+
+            var controller = new HomeController();
+            var actual_string = controller.ConfiguredAction(id);
+
+            Assert.Equal(actual_string, expected_string);
+        }
+    }
+}
