f40919d7d2b81312558a29be0527cb91c1549ce6
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-02-06 18:34:54 +0700

    Комментарии к коду

--- Services/WebStore.Interfaces/Services/ICartStore.cs
+++ Services/WebStore.Interfaces/Services/ICartStore.cs
@@ -3,8 +3,14 @@ using WebStore.Domain.Entities;
 
 namespace WebStore.Interfaces.Services
 {
+    /// <summary>
+    /// Хранилище данных корзины
+    /// </summary>
     public interface ICartStore
     {
+        /// <summary>
+        /// объект корзины
+        /// </summary>
         public Cart Cart { get; set; }
     }
 }
