20875160486a44518d2a367243963ee709ec5ad8
orel13001 <68144818+orel13001@users.noreply.github.com>
2022-02-10 22:02:50 +0700

    Карта сайта

---
+++ UI/WebStore/Controllers/API/SiteMapController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers.API
+{
+	public class SiteMapController : ControllerBase //localhost/SiteMap
+	{
+		public IActionResult Index([FromServices] IProductData ProductData)
+		{
+			var nodes = new List<SitemapNode>
+			{
+				new (Url.Action("Index", "Home")),
+				new (Url.Action("ConfiguredAction", "Home")),
+				new (Url.Action("Index", "Blogs")),
+				new (Url.Action("Blog", "Blogs")),
+				new (Url.Action("Index", "WebAPI")),
+				new (Url.Action("Index", "Catalog")),
+			};
+
+			nodes.AddRange(ProductData.GetSections().Select(s => new SitemapNode(Url.Action("Index", "Catalog", new {SectionId = s.Id}))));
+			nodes.AddRange(ProductData.GetBrands().Select(b => new SitemapNode(Url.Action("Index", "Catalog", new {BrandId = b.Id}))));
+			nodes.AddRange(ProductData.GetProducts().Select(p => new SitemapNode(Url.Action("Index", "Catalog", new {p.Id}))));
+
+
+			return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+		}
+	}
+}
